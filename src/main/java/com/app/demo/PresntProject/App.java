4cf55346981c8b4b8afda3860baaab88e19d3102
Jalukurisamsritha <jalikurisamsritha1718@gmail.com>
2023-11-02 16:05:57 +0530

    Demo for Team

---
+++ src/main/java/com/app/demo/PresntProject/App.java
@@ -0,0 +1,13 @@
+package com.app.demo.PresntProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
